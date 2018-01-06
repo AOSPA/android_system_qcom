8311bdd7a34ef8bcd7e607b8d59b35ff08ed0c48
Hernán Castañón <herna@paranoidandroid.co>
2018-01-06 17:51:22 +0100

    softap: add liblog
    Change-Id: Icfdba36189e483f4ef0f24821c5faad615c2f2b7
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- softap/jni/Android.mk
+++ softap/jni/Android.mk
@@ -9,6 +9,8 @@ LOCAL_SRC_FILES := QWiFiSoftApCfg.c
 
 LOCAL_MODULE := libQWiFiSoftApCfg
 
+LOCAL_LDLIBS := -llog
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
